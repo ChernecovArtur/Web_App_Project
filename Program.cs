1d8f958294d76e6037e9f1049cde3030bea4bf42
shabalin-AA <92672124+shabalin-AA@users.noreply.github.com>
2022-11-06 13:37:24 +0500

    Add files via upload
    Program.cs файл с чтением excel

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ClosedXML.Excel;
+
+namespace excel
+{
+    class Program
+    {
+        static void ReadExcel(string path)
+        {
+            using XLWorkbook workbook = new XLWorkbook(path);
+            var sheet = workbook.Worksheet(1);
+            for (int i = 1; i < 76; i++)
+            {
+                for (char charIndex = 'a'; charIndex < 'd'; charIndex++)
+                {
+                    string index = charIndex + i.ToString();
+                    string data = sheet.Cell(index).GetValue<string>();
+                    Console.WriteLine(data);
+                }
+            }
+        }
+
+        static void Main()
+        {
+            ReadExcel("test.xlsx");
+        }
+    }
+}
