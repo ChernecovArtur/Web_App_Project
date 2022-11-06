1b81965efc2228e9aad268eeb734ad44554120ea
ChernecovArtur <chernecov_artur@mail.ru>
2022-11-06 12:20:02 +0500

    Add First Version

---
+++ Web_App/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+namespace excel
+{
+    class Program
+    {
+        static void ReadExcel(string path)
+        {
+            ISheet sheet;
+            using (var stream = new FileStream(path, FileMode.Open))
+            {
+                stream.Position = 0;
+                XSSFWorkbook xssWorkbook = new XSSFWorkbook(stream);
+
+                sheet = xssWorkbook.GetSheetAt(0);
+
+                for (int i = sheet.FirstRowNum; i <= sheet.LastRowNum; i++)
+                {
+                    Console.Write('\n');
+
+                    IRow row = sheet.GetRow(i);
+                    for (int j = row.FirstCellNum; j < row.LastCellNum; j++)
+                    {
+                        if (row.GetCell(j) != null && row.GetCell(j).ToString() != null)
+                        {
+                            Console.Write(row.GetCell(j).ToString() + ' ');
+                        }
+                    }
+                }
+            }
+        }
+
+        static void Main()
+        {
+            ReadExcel("test.xlsx");
+        }
+    }
+}
