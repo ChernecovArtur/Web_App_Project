9d5ed24ff0f981d9b23d4e32a1c1f11482980e8a
AzmanovaEvelina <93144576+AzmanovaEvelina@users.noreply.github.com>
2022-11-20 21:54:04 +0500

    getting started with ASP.NET Core

---
+++ Веб-приложение/Веб-приложение_РАСПИСАНИЕ/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Веб_приложение_РАСПИСАНИЕ.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Message { get; set; }
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Message = "";
+        }
+        public void OnPost(string? name_group)
+        {
+            Message = $"Введенное название: {name_group}";
+        }
+    }
+}
